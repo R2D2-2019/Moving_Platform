7735fc933a5e1e2fde020447895442d7b1455e62
ThePatman3 <patrick.vanderbend@student.hu.nl>
2019-04-18 11:12:06 +0200

    Formatted the code according to the style guide

--- code/main.cpp
+++ code/main.cpp
@@ -1,35 +1,28 @@
 #include "hwlib.hpp"
-#include <Qik2s12v10.hpp>
-// helper function for debugging
-void sendAt9600(uint8_t byte, hwlib::target::pin_out txPin){
-  txPin.write(0); // low startbit
-  hwlib::wait_ns(104166);
-  for(unsigned int i = 0; i<8; i++){
-    txPin.write((byte>>i) & 1);
-    hwlib::wait_ns(104166);
-  }
-  txPin.write(1); // high endbit
-  hwlib::wait_ns(104166);
-}
+#include <qik_2s12v10.hpp>
 
 int main(void) {
   // kill the watchdog
   WDT->WDT_MR = WDT_MR_WDDIS;
   hwlib::wait_ms(1000);
 
-  // Qik2s12v10 testing
+  // qik_2s12v10_c testing
 
   auto motorControllerReset = hwlib::target::pin_out(2,25);
   
-  r2d2::moving_platform::Qik2s12v10 motorController(r2d2::uart_ports_c::uart1, 9600u, &motorControllerReset);
+  r2d2::moving_platform::qik_2s12v10_c motorController(r2d2::uart_ports_c::uart1, 9600u, &motorControllerReset);
   hwlib::cout << "initializing!\n";
   motorController.init();
   hwlib::cout << "initialized!\n";
-  
+
+  hwlib::cout << "Current PWM configuration: " << motorController.get_configuration_parameter(1) << '\n';
+
   while(true){
     motorController.set_speed(100);
     hwlib::wait_ms(2000);
     motorController.set_speed(0);
+    hwlib::wait_ms(2000);
+    motorController.set_speed(-100);
     hwlib::wait_ms(2000);
   }
 }
