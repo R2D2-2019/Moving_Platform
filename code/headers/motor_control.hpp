a730731c0ea94ab4998a06bcaf3c21f89edc4bfa
vera98x <v.schoonderwoerd@outlook.com>
2019-05-10 12:31:06 +0200

    code updated according style guide

--- code/headers/motor_control.hpp
+++ code/headers/motor_control.hpp
@@ -1,16 +1,16 @@
 #pragma once
-#include "hwlib.hpp"
+#include <hwlib.hpp>
 
 namespace r2d2::moving_platform {
 
     class motor_control_c {
-    protected:
-        signed int8_t speed;
+    private:
+        int8_t speed;
 
     public:
-        virtual void set_speed(const int8_t &_speed) = 0;
+        virtual void set_speed(int8_t new_speed) = 0;
 
-        inline int8_t get_speed() const {
+        int8_t get_speed() const {
             return speed;
         }
     };
