94bc4661e5435f439559a98a4dd60e25c072669a
vera98x <v.schoonderwoerd@outlook.com>
2019-05-15 21:54:57 +0200

    merge fix for PR (removed double constructor and made private protected in moving_platform

---
+++ code/src/moving_platform.cpp
@@ -0,0 +1,13 @@
+#include <moving_platform.hpp>
+
+namespace r2d2::moving_platform {
+
+    int8_t moving_platform_c::get_speed() const {
+        return speed;
+    }
+		
+    int16_t moving_platform_c::get_steering() const {
+        return steering_angle;
+    }
+
+}
