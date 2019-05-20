2e4799ddcd46db0552a5b333f5eba54f899d06b2
ruben <rubenrara@gmail.com>
2019-05-20 02:53:43 +0200

    added the l9110 class wich control the l9110 dc motor channel

---
+++ code/headers/l9110.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <hwlib.hpp>
+#include <pwm.hpp>
+
+using namespace R2D2::pwm_lib;
+
+namespace R2D2 {
+    namespace moving_platform {
+        /* @brief
+         *this class controls the l9110 2dc channel motor driver to controll the
+         *dc motor of the turtle.
+         */
+        class l9110 {
+        private:
+            int16_t speed = 0; // motor is idle by default
+            pwm_c &channel_fowrard, &channel_backward;
+
+        public:
+            /*
+             *@brief
+             *constucts l9110
+             *@param  pwm_c: pwm channel_fowrard for pin IA
+             *@param  pwm_c: pwm channel_backward for pin IB
+             */
+            l9110(pwm_c &channel_fowrard, pwm_c &channel_backward);
+
+            /*
+             *@brief
+             *set new motor speed
+             *@detail
+             * set motor, speed cna be set from -255 to 255.
+             * if speed is set below 0 then the motor will rototate backward
+             */
+            void set_speed(int16_t new_speed);
+            /*
+             *@brief
+             *get the value of what the motor speed is set;
+             *@return int: speed of motor
+             */
+            int16_t get_speed();
+        };
+    } // namespace moving_platform
+} // namespace R2D2
---
+++ code/src/l9110.cpp
@@ -0,0 +1,26 @@
+#include <l9110.hpp>
+
+using namespace R2D2::pwm_lib;
+namespace R2D2::moving_platform {
+
+    l9110::l9110(pwm_c &channel_fowrard, pwm_c &channel_backward)
+        : channel_fowrard(channel_fowrard), channel_backward(channel_backward) {
+    }
+
+    void l9110::set_speed(int16_t new_speed) {
+        speed = new_speed;
+        // check if motor has to be set fotward of backward
+        if (speed >= 0) {
+            channel_fowrard.set_duty_cycle(speed);
+            channel_backward.set_duty_cycle(0);
+        } else {
+            channel_backward.set_duty_cycle(-1 * speed);
+            channel_fowrard.set_duty_cycle(0);
+        }
+    }
+
+    int16_t l9110::get_speed() {
+        return speed;
+    }
+
+} // namespace R2D2::moving_platform
