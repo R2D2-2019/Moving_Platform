78999e0250d30a6be2f256d46622af15e4f61217
Joepieler <be.campagne@live.nl>
2019-06-13 15:10:13 +0200

    First test with encoders. The code is tested on the Beetle. Beetle can now turn 90 degrees with encoders. Code needs to go to the right class.

--- code/main.cpp
+++ code/main.cpp
@@ -11,40 +11,69 @@ int main(void) {
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
 
-    // canbus beetle testing
-    bool beetle_canbus = false;
-    // beetle testing
-    bool test_set_speed = true;
-
+    auto encode_m0 = hwlib::target::pin_adc(hwlib::target::ad_pins::a0);
+    auto encode_m1 = hwlib::target::pin_adc(hwlib::target::ad_pins::a1);
     auto qik_2s12v10_reset_pin = hwlib::target::pin_out(2, 25); // digital pin 5
     auto usart = r2d2::usart::hardware_usart_c<r2d2::usart::usart0>(9600);
     r2d2::moving_platform::qik_2s12v10_c qik =
         r2d2::moving_platform::qik_2s12v10_c(usart, qik_2s12v10_reset_pin);
 
-    r2d2::comm_c comm;
-    auto beetle = r2d2::moving_platform::beetle_c(qik, comm);
+    /*
+     * Test to turn the beetle 90 degrees.
+     * code needs a go to the right class(beetle and rhino)
+     */
+    // Turn value, the wheels need to turn 0.62 for 90 degrees. 1 turn is one
+    // full rotation of the wheel.
+    float turn = 0.62;
+    hwlib::wait_ms(100);
+    // The puls. Its starts low.
+    bool low_m0 = false;
+    int counter_m0 = 0;
+    bool low_m1 = false;
+    int counter_m1 = 0;
+    // The adc input. is between 3000 and 3800.
+    unsigned int adc_voltage = 3500;
+    // Encode frequency for 1 turn of the wheel. the encoder has 48 point per
+    // rotation 16.6. the total is then 796.8
+    int encode_1_full_turn = 797;
+    // Motor speed
+    int motor_speed = 20;
+    // motor tests strats motors.
+    hwlib::cout << "start test \n";
+    hwlib::wait_ms(10);
+    qik.set_m0_speed(motor_speed);
+    qik.set_m1_speed(motor_speed);
 
-    if (beetle_canbus) {
-        while (1) {
-            beetle.process();
-            hwlib::wait_ms(100);
+    while (true) {
+        if (encode_m0.read() > adc_voltage) {
+            if (low_m0 == true) {
+                counter_m0++;
+            }
+            low_m0 = false;
+        } else {
+            low_m0 = true;
+        }
+        if (counter_m0 == (int(encode_1_full_turn * turn))) {
+            qik.brake_m0(motor_speed);
+        }
+
+        if (encode_m1.read() > adc_voltage) {
+            if (low_m1 == true) {
+                counter_m1++;
+            }
+            low_m1 = false;
+        } else {
+            low_m1 = true;
+        }
+        if (counter_m1 == (int(encode_1_full_turn * turn))) {
+            qik.brake_m1(motor_speed);
+        }
+        if (counter_m0 > (encode_1_full_turn * turn) &&
+            counter_m1 > (encode_1_full_turn * turn)) {
+            break;
         }
     }
 
-    hwlib::wait_ms(100);
-
-    if (test_set_speed) {
-        // motor tests:
-        hwlib::cout << "start \n";
-        beetle.set_speed(100);
-        beetle.turn(0);
-        hwlib::wait_ms(1000);
-        beetle.set_speed(50);
-        hwlib::cout << "Testing both motors, " << qik.get_m0_speed() << "% ."
-                    << qik.get_m1_speed() << "% power forward.\n";
-        hwlib::wait_ms(5000);
-        beetle.set_speed(0);
-    }
     hwlib::cout << "All tests have been completed\n";
 
     return 0;
