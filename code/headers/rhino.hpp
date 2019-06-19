b3e17c708c51e1753263afbc8dd9c101563ecc0f
Cris <crisvdnolle@hotmail.com>
2019-06-19 23:22:13 +0200

    Implemented turn function with encoders
    The rhino class is now compatible with the new updated UML class diagram. The rhino now uses the encoders to turn in a circle.

--- code/headers/rhino.hpp
+++ code/headers/rhino.hpp
@@ -11,15 +11,17 @@ namespace r2d2::moving_platform {
         qik_2s12v10_c qik_2s12v10_motorcontroller;
         hwlib::adc &encode_m0;
         hwlib::adc &encode_m1;
+        constexpr static uint8_t turn_factor = 66;
+        uint8_t turn_speed = 80;
 
     public:
         /**
          * @brief
-         * Constructor beetle_c
-         * @param    uart_port: r2d2::uart_ports_c
-         * @param    baud_rate: unsigned int
-         * @param    reset_pin: hwlib::pin_out &
-         * @param    comm:      base_comm_c &
+         * Constructor rhino_c
+         * @param    qik_2s12v10_motorcontroller
+         * @param    comm
+         * @param    encode_m0
+         * @param    encode_m1
          */
         rhino_c(qik_2s12v10_c &qik_2s12v10_motorcontroller, base_comm_c &comm,
                 hwlib::adc &encode_m0, hwlib::adc &encode_m1);
@@ -28,40 +30,26 @@ namespace r2d2::moving_platform {
          * @brief
          * This function will set the speed between -100 and 100 % speed
          * @detail
-         * the speed set must not exceed -100 of 100, if it is, it will set back
-         * to 100 or -100
-         * @param    int: new speed percentage
+         * the speed set must not exceed -127 of 127, if it is, it will set back
+         * to 127 or -127
+         * @param    new_speed
          */
         void set_speed(int8_t new_speed) override;
 
         /**
          * @brief
-         * This function will set the steering between -90 and 90 degrees
-         * @param    int: new degrees
-         */
-        void set_steering(int16_t degrees) override;
-
-        /**
-         * @brief
-         * This function will turn the beetle between -90 and 90
+         * This function will turn the rhino.
          * @detail
-         * The degrees set must not exceed -90 of 90, if it is, it will set back
-         * to -90 or 90
-         * @param    int: new degrees
+         * The rhino will turn while stationary. A value of 0 to 180 degrees is
+         * tested to be accurate.
+         * @param   degrees
          */
         void turn(int16_t degrees) override;
         /**
          * @brief
          * This function will move the platform a certain distance
-         * @param    int: distance
+         * @param    distance
          */
         void move(int8_t distance) override;
-        /**
-         * @brief
-         * This function will move the platform following a forwards and a
-         * backwards
-         * @param    int: distance
-         */
-        void move(int8_t x, int8_t y) override;
     };
 } // namespace r2d2::moving_platform
--- code/src/rhino.cpp
+++ code/src/rhino.cpp
@@ -15,10 +15,10 @@ namespace r2d2::moving_platform {
         // (foreward))
         speed = new_speed;
         // do not go out of range
-        if (speed > 100) {
-            speed = 100;
-        } else if (speed < -100) {
-            speed = -100;
+        if (speed > 127) {
+            speed = 127;
+        } else if (speed < -127) {
+            speed = -127;
         }
         // because of inaccuracies, when not touching the pendals of manual
         // control, a number beween -3 and 8 can be given Just to make sure the
@@ -27,24 +27,12 @@ namespace r2d2::moving_platform {
         else if (speed < 10 && speed > -10) {
             speed = 0;
         }
-        qik_2s12v10_motorcontroller.set_m0_speed(-1 * speed);
+        qik_2s12v10_motorcontroller.set_m0_speed(speed);
         qik_2s12v10_motorcontroller.set_m1_speed(speed);
     }
 
-    void rhino_c::set_steering(int16_t degrees) {
-    }
     void rhino_c::turn(int16_t degrees) {
-        // because of inaccuracies, when moving forward, the steer of manual
-        // control will give a number beween -10 and 10 Just to make sure the
-        // robot will move forward and not react to quickly, a theshold is made
-        // for -15 till 15.
-        int min_degrees = 15;
-        int max_degrees = 90;
-        if ((degrees < min_degrees && degrees > -min_degrees) ||
-            degrees > max_degrees || degrees < -max_degrees) {
-            degrees = 0;
-        }
-        float turn = 70;
+
         // The puls. Its starts low.
         bool low_m0 = false;
         int counter_m0 = 0;
@@ -56,18 +44,16 @@ namespace r2d2::moving_platform {
         // per over 2 pins we count when de adc goes from low to high of 1 pin.
         // The gear ratio from the motor is 50:1 64/4*50 = 800
         int encode_1_full_turn = 800;
-        // Motor speed
-        // Default turn speed.
-        int motor_speed = 80;
-        // Turn the right way.
+
+        // Invert the motor speed and degrees if its a negative value.
         if (degrees < 0) {
             // turn right
-            motor_speed = -motor_speed;
-            degrees = -degrees;
+            turn_speed = turn_speed * -1;
+            degrees = degrees * -1;
         }
         // motor tests strats motors.
-        qik_2s12v10_motorcontroller.set_m0_speed(motor_speed);
-        qik_2s12v10_motorcontroller.set_m1_speed(motor_speed);
+        qik_2s12v10_motorcontroller.set_m0_speed(turn_speed);
+        qik_2s12v10_motorcontroller.set_m1_speed(-1 * turn_speed);
 
         while (true && degrees != 0) {
             if (encode_m0.read() > adc_voltage) {
@@ -78,10 +64,6 @@ namespace r2d2::moving_platform {
             } else {
                 low_m0 = true;
             }
-            if (counter_m0 ==
-                (int(encode_1_full_turn * turn / 360 * degrees))) {
-                qik_2s12v10_motorcontroller.brake_m0(20);
-            }
 
             if (encode_m1.read() > adc_voltage) {
                 if (low_m1 == true) {
@@ -91,21 +73,25 @@ namespace r2d2::moving_platform {
             } else {
                 low_m1 = true;
             }
-            if (counter_m1 ==
-                (int(encode_1_full_turn * turn / 360 * degrees))) {
-                qik_2s12v10_motorcontroller.brake_m1(20);
+
+            if (counter_m0 ==
+                (int(encode_1_full_turn * turn_factor / 360 * degrees))) {
+                qik_2s12v10_motorcontroller.set_m0_speed(0);
             }
-            if (counter_m0 > (encode_1_full_turn * turn / 360 * degrees) &&
-                counter_m1 > (encode_1_full_turn * turn / 360 * degrees)) {
+            if (counter_m1 ==
+                (int(encode_1_full_turn * turn_factor / 360 * degrees))) {
+                qik_2s12v10_motorcontroller.set_m1_speed(0);
+            }
+            if (counter_m0 >
+                    (encode_1_full_turn * turn_factor / 360 * degrees) &&
+                counter_m1 >
+                    (encode_1_full_turn * turn_factor / 360 * degrees)) {
                 break;
             }
-            // wait so the while loop aint blocking
-            hwlib::wait_ms(0.1);
         }
     }
 
     void rhino_c::move(int8_t distance) {
     }
-    void rhino_c::move(int8_t x, int8_t y) {
-    }
+
 } // namespace r2d2::moving_platform
