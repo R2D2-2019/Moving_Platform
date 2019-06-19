f7ed511f4261cff7fa32a85dc8dc0145bf035934
Cris <crisvdnolle@hotmail.com>
2019-06-19 23:24:17 +0200

    Edited main test loop
    Test loop for rhino turn is inside this main. Added documentation for each line what is done.

--- code/main.cpp
+++ code/main.cpp
@@ -9,19 +9,31 @@ int main(void) {
     // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
+
+    // Bool to change between process function and rhino turn test.
     bool rhino_motor_test = true;
+
+    // Create two encode pins for the motors.
     auto encode_m0 = hwlib::target::pin_adc(hwlib::target::ad_pins::a0);
     auto encode_m1 = hwlib::target::pin_adc(hwlib::target::ad_pins::a1);
-    auto qik_2s12v10_reset_pin = hwlib::target::pin_out(2, 25); // digital pin 5
-    auto usart = r2d2::usart::hardware_usart_c<r2d2::usart::usart0>(9600);
 
+    // Create reset pin for the qik board
+    auto qik_2s12v10_reset_pin = hwlib::target::pin_out(2, 25); // digital pin 5
+
+    // Create usart object.
+    auto usart = r2d2::usart::hardware_usart_c<r2d2::usart::usart0>(11500);
+
+    // Create qik_controller for robot
     r2d2::moving_platform::qik_2s12v10_c qik =
         r2d2::moving_platform::qik_2s12v10_c(usart, qik_2s12v10_reset_pin);
+
     // create internal commmunication
     r2d2::comm_c comm;
 
+    // Construct rhino object
     auto rhino =
         r2d2::moving_platform::rhino_c(qik, comm, encode_m0, encode_m1);
+
     if (!rhino_motor_test) {
         while (1) {
             rhino.process();
@@ -30,31 +42,13 @@ int main(void) {
     }
 
     if (rhino_motor_test) {
-        // hwlib::cout << "Testing both motors, 31% power forward.\n";
-        // rhino.set_speed(80);
-        // rhino.turn(0);
-        // hwlib::wait_ms(1000);
-        // rhino.set_speed(0);
-
-        // hwlib::wait_ms(500);
-
-        // hwlib::cout << "Testing both motors, 31% power backward.\n";
-        // rhino.set_speed(-80);
-        // rhino.turn(0);
-        // hwlib::wait_ms(1000);
-        // rhino.set_speed(0);
-
+        hwlib::cout << "Testing the rhino to turn 90 degrees left and right.\n";
         hwlib::wait_ms(500);
-        hwlib::cout << "Testing turning 90 degrees left.\n";
-        //        rhino.set_speed(40);
-        rhino.turn(-90);
-        hwlib::wait_ms(3000);
-
         rhino.turn(90);
-        hwlib::wait_ms(3000);
+        hwlib::wait_ms(1'000);
+        rhino.turn(-90);
+        hwlib::wait_ms(1'000);
     }
-
-    hwlib::cout << "All tests have been completed\n";
 
     return 0;
 }
