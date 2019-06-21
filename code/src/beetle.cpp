e2e68ff8c98ac8a833490fa16e4e81e2f7b93bd3
Joepieler <be.campagne@live.nl>
2019-06-21 13:15:53 +0200

    Test move 1 meter in a straight line

--- code/src/beetle.cpp
+++ code/src/beetle.cpp
@@ -109,69 +109,69 @@ namespace r2d2::moving_platform {
         // Encode frequency for 1 turn of the wheel. the encoder has 64 point
         // per over 2 pins we count when de adc goes from low to high of 1 pin.
         // The gear ratio from the motor is 50:1 64/4*50 = 800
-        int rotation = distance / 39 * 800;
+        const int16_t rotation = (distance - 3) * 800 / 39;
+        hwlib::cout << rotation << '\n';
         // The encoder code checks if the pulse goes from low to high. This is
         // why we start the bool low.
         bool low_m0 = true;
         int counter_m0 = 0;
-        int counter_m0_totaal = 0;
+        int16_t counter_m0_total = 0;
         bool low_m1 = true;
         int counter_m1 = 0;
-        int counter_m1_totaal = 0;
+        int16_t counter_m1_total = 0;
         // The adc input. is between 3000 and 3800.
         unsigned int adc_voltage = 3500;
-        int master_speed = -50;
-        int slave_speed = master_speed + 5;
-        qik_2s12v10_motorcontroller.set_m0_speed(master_speed);
-        qik_2s12v10_motorcontroller.set_m1_speed(slave_speed);
+        int master_speed = 25;
+        int slave_speed = master_speed - 5;
+        qik_2s12v10_motorcontroller.set_m1_speed(-slave_speed);
+        qik_2s12v10_motorcontroller.set_m0_speed(-master_speed);
         // PID
         int error = 0;
-        int kp = 10;
+        int kp = 2;
         int tick = hwlib::now_us();
 
         while (true) {
             if (motor_encoder_m0.read() > adc_voltage) {
                 if (low_m0 == true) {
                     counter_m0++;
-                    counter_m0_totaal++;
+                    counter_m0_total++;
                 }
                 low_m0 = false;
             } else {
                 low_m0 = true;
             }
-            if (counter_m0_totaal == rotation) {
-                qik_2s12v10_motorcontroller.brake_m0(100);
+            if (counter_m0_total == rotation) {
+                qik_2s12v10_motorcontroller.brake_m0(0);
             }
 
             if (motor_encoder_m1.read() > adc_voltage) {
                 if (low_m1 == true) {
                     counter_m1++;
-                    counter_m1_totaal++;
+                    counter_m1_total++;
                 }
                 low_m1 = false;
             } else {
                 low_m1 = true;
             }
 
-            if (counter_m1_totaal == rotation) {
-                qik_2s12v10_motorcontroller.brake_m1(100);
-            } else if (counter_m1_totaal < rotation) {
-                slave_speed = ((slave_speed <= -10) ? slave_speed : -10);
-                qik_2s12v10_motorcontroller.set_m1_speed(slave_speed);
+            if (counter_m1_total == rotation) {
+                qik_2s12v10_motorcontroller.brake_m1(0);
+            } else if (counter_m1_total < rotation) {
+                slave_speed = ((slave_speed > 10) ? slave_speed : 10);
+                qik_2s12v10_motorcontroller.set_m1_speed(-slave_speed);
             }
 
-            if (counter_m0_totaal > rotation && counter_m1_totaal > rotation) {
-                hwlib::cout << "Done \n";
+            if (counter_m0_total > rotation && counter_m1_total > rotation) {
+                hwlib::cout << "Done " << rotation << "\n";
                 break;
 
                 // Master slave method
             } else {
-                if (hwlib::now_us() - tick > 10000) {
+                if (hwlib::now_us() - tick > 50000) {
                     error = counter_m0 - counter_m1;
-                    slave_speed -= error / kp;
+                    slave_speed += error / kp;
                     counter_m0 = 0;
                     counter_m1 = 0;
-
                     tick = hwlib::now_us();
                 }
             }
